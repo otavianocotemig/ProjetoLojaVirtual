ac96b2e741ad67eccdd6605a124e08ddbe1c81f2
otavianocotemig <otaviano@cotemig.com.br>
2020-10-02 09:02:33 -0300

    .

--- ProjetoLojaVirtual/BLL/tblClienteBLL.cs
+++ ProjetoLojaVirtual/BLL/tblClienteBLL.cs
@@ -61,52 +61,7 @@ namespace ProjetoLojaVirtual.BLL
             daoBanco.ExecutarComando(sql);
         }
 
-        // Criação de Metodo para selecionar os dados do cliente - Polimorfismo 
-        public DataTable ListarClientes(string email)
-        {
-            string sql = string.Format($@"select * from tbl_cliente where email_cliente = '{email}';");
-            return daoBanco.ExecutarConsulta(sql);
-        }
-        public DataTable ListarClientes()
-        {
-            string sql = string.Format($@"select * from tbl_cliente");
-            return daoBanco.ExecutarConsulta(sql);
-        }
-        // metodo utilizado para alterar dados do cliente
-        public void AlterarCliente(tblClienteDTO DtoCliente)
-        {
-            string sql = string.Format($@"UPDATE tbl_cliente set nome_cliente = '{DtoCliente.Nome_cliente}',
-                                                                 sobrenome_cliente = '{DtoCliente.Sobrenome_cliente}',
-                                                                 cpf_cliente = '{DtoCliente.Cpf_cliente}',
-                                                                 senha_cliente = '{DtoCliente.Senha_cliente}'
-                                                   where email_cliente = '{DtoCliente.Email_cliente}';");
-            daoBanco.ExecutarComando(sql);
-        }
-
-        //Metodo para Inserir Cliente no Banco de Dados
-        public void InserirCliente(tblClienteDTO ObjCliente)
-        {
-            string sql = string.Format($@"INSERT INTO tbl_cliente VALUES (NULL, '{ObjCliente.Nome_cliente}',
-                                                                                '{ObjCliente.Sobrenome_cliente}',
-                                                                                '{ObjCliente.Email_cliente}',
-                                                                                '{ObjCliente.Senha_cliente}',
-                                                                                '{ObjCliente.Cpf_cliente}',    
-                                                                                '{ObjCliente.Tp_usuario}');");
-            daoBanco.ExecutarComando(sql);
-        }
-        // Metodo utilizado para excluir Cliente no Banco
-        public void ExcluirCliente(tblClienteDTO objCliente)
-        {
-            string sql = string.Format($@"DELETE FROM tbl_cliente where id_cliente = {objCliente.Id_cliente};");
-            daoBanco.ExecutarComando(sql);
-        }
-       
-        // Metodo para Consultar Clientes no Banco
-        public DataTable PesquisarClientes(string condicao)
-        {
-            string sql = string.Format($@"select * from tbl_cliente where " + condicao);
-            return daoBanco.ExecutarConsulta(sql);
-        }
+    
 
     }
    
